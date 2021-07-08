4c7dab27b436c0d64a77b307850cc50667a1fc97
kaishajri <rafik_alex53@outlook.fr>
2021-07-08 19:41:47 +0200

    les entet de la pile

---
+++ pile.h
@@ -0,0 +1,11 @@
+struct element
+{
+    int cle;
+    struct element *suivant
+};
+
+void cree(struct element**);
+unsigned vide(struct element*);
+int dernier(struct element*);
+void empiler(int,struct element**);
+void depiler(struct element**);
