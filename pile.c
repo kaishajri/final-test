29266305cde1cb4ec0f66b07d517d47eb25b05bb
kaishajri <rafik_alex53@outlook.fr>
2021-07-08 19:47:07 +0200

    les implementation

---
+++ pile.c
@@ -0,0 +1,26 @@
+#include "pile.h"
+#include <malloc.h>
+#include <stdlib.h>
+#include <assert.h>
+
+void cree(struct element **p)
+{
+    *p=(struct element*)malloc(sizeof(struct element));
+    *p=NULL;
+}
+unsigned vide(struct element *p)
+{
+    return p==NULL;
+}
+int dernier(struct element *p)
+{
+    return p->cle;
+}
+void empiler(int x,struct element **p)
+{ 
+    struct element *e=(struct element*)malloc(sizeof(struct element));
+    e->cle=x;
+    e->suivant=*p;
+    *p=e;
+}
+void depiler(struct element**);
